556e9ba956553a76d97a3a2b5726d4d0997be312
DavGuett <soulsec@protonmail.com>
2023-06-01 22:11:08 -0300

    Dapper functional

--- DataAccess/Program.cs
+++ DataAccess/Program.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.Data.SqlClient;
-const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=PBRy6aMwXyN7tq";
+﻿using Dapper;
+using DataAccess.Models;
+using Microsoft.Data.SqlClient;
+const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=PBRy6aMwXyN7tq;Trusted_Connection=False; TrustServerCertificate=True";
 
 using(var connection = new SqlConnection(connectionString))
 {
-    Console.WriteLine("Connected");
+   var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+    foreach(var category in categories)
+    {
+        Console.WriteLine($"{category.Id} - {category.Title}");
+    }
 }
 Console.WriteLine("Hello, World!");
