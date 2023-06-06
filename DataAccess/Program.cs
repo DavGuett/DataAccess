48ea56ae5c1097fde9872f33aedf08983ea7f8c3
DavGuett <soulsec@protonmail.com>
2023-06-05 21:10:09 -0300

    Separated the select query into a function

--- DataAccess/Program.cs
+++ DataAccess/Program.cs
@@ -3,12 +3,16 @@ using DataAccess.Models;
 using Microsoft.Data.SqlClient;
 const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=PBRy6aMwXyN7tq;Trusted_Connection=False; TrustServerCertificate=True";
 
-using(var connection = new SqlConnection(connectionString))
+using (var connection = new SqlConnection(connectionString))
 {
-   var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-    foreach(var category in categories)
+   
+}
+
+static void ListCategories(SqlConnection connection)
+{
+    var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+    foreach (var item in categories)
     {
-        Console.WriteLine($"{category.Id} - {category.Title}");
+        Console.WriteLine($"{item.Id} - {item.Title}");
     }
 }
-Console.WriteLine("Hello, World!");
